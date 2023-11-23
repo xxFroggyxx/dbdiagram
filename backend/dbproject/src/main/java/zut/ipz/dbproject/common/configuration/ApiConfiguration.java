cca074b3b82202cc78cd04fe3f0308bb47ad4659
1Batrex1 <bartosz.gajda@protonmail.com>
2023-11-23 18:02:32 +0100

    ADD Documentation to ApiConfiguration

--- backend/dbproject/src/main/java/zut/ipz/dbproject/common/configuration/ApiConfiguration.java
+++ backend/dbproject/src/main/java/zut/ipz/dbproject/common/configuration/ApiConfiguration.java
@@ -4,15 +4,27 @@ package zut.ipz.dbproject.common.configuration;
 import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This class contains all the constants for the API.
+ * Use this class to avoid hardcoding the API paths.
+ */
 @Configuration
 @NoArgsConstructor
 public class ApiConfiguration {
 
 
-
+    /**
+     * This is the base path for the API.
+     */
     public static final String API = "/api/v1";
 
+    /**
+     * This is the base path for the test endpoint.
+     */
     public static final String TEST = "/test";
 
+    /**
+     * This is the base path for the parser endpoint.
+     */
     public static final String PARSER = "/parser";
 }
