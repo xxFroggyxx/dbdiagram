ebae0c0b112a32db1308aab8f95266b958a0a2c0
1Batrex1 <bartosz.gajda@protonmail.com>
2023-11-23 17:07:02 +0100

    ADD Basic api configuration file

---
+++ backend/dbproject/src/main/java/zut/ipz/dbproject/common/configuration/ApiConfiguration.java
@@ -0,0 +1,18 @@
+package zut.ipz.dbproject.common.configuration;
+
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@NoArgsConstructor
+public class ApiConfiguration {
+
+
+
+    public static final String API = "/api/v1";
+
+    public static final String TEST = "/test";
+
+    public static final String PARSER = "/parser";
+}
