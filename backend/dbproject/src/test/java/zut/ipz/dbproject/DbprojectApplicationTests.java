e4e197047878bc4fd0bc338bbc05a27fbe64761e
1Batrex1 <bartosz.gajda@protonmail.com>
2023-11-23 16:22:15 +0100

    ADD basic project files

---
+++ backend/dbproject/src/test/java/zut/ipz/dbproject/DbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package zut.ipz.dbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
