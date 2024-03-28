e73db3650230b06825d58f5ce7d3c933ec4916f8
Damian <damianzajac01@wp.pl>
2024-03-28 06:23:56 +0100

    Code cleanup

--- backend/dbproject/src/main/java/zut/ipz/dbproject/file/FileUtils.java
+++ backend/dbproject/src/main/java/zut/ipz/dbproject/file/FileUtils.java
@@ -9,27 +9,14 @@ import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
 public class FileUtils {
-    /**
-     * This method reads a file and returns a list of strings.
-     *
-     * @param sqlFile is a file that will be read
-     * @return a list of strings
-     */
     public static List<String> getLines(MultipartFile sqlFile) {
         List<String> lines;
+
         try (var br = new BufferedReader(new InputStreamReader(sqlFile.getInputStream()))) {
             lines = br.lines().toList();
-
         } catch (IOException e) {
             throw new IllegalFormatCodePointException(2);
         }
-
-//        List<String> lines = null;
-//        try (Stream<String> streamLines = Files.lines(Path.of(sqlFile.getName()))) {
-//            lines = streamLines.toList();
-//        } catch (IOException e) {
-//            System.err.println(e.getMessage());
-//        }
 
         return lines;
     }
